56ca19366a7312838f1f8b619df004631e529899
Udit Bhatia <uditbhatia121@gmail.com>
2025-12-14 14:30:04 +0000

    use openrouter instead

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,11 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.5-flash',
+  plugins: [
+    googleAI({
+      apiKey: process.env.OPENROUTER_API_KEY || '',
+      // baseUrl: 'https://openrouter.ai/api/v1',
+    }),
+  ],
+  // model: 'openai/gpt-3.5-turbo',
 });
